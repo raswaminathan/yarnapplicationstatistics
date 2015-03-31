55f3f7726760c8416c3a5222aa8d3a085d451395
Brett Fox <bsf7@duke.edu>
2015-03-31 02:36:53 -0400

    Added an SQL wrapper

---
+++ src/main/java/com/rahulswaminathan/yarnapplicationstatistics/SQLWrapper.java
@@ -0,0 +1,143 @@
+package com.rahulswaminathan.yarnapplicationstatistics;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLWrapper {
+
+	Connection conn = null;
+
+	/**
+	 * A wrapper for MySQL. This can simplify writing to a MySQL database.
+	 * 
+	 * @param databaseName
+	 *            Name of the database being written to.
+	 * @param host
+	 *            Host of the MySQL server. Typically localhost.
+	 * @param user
+	 *            Username for the MySQL server.
+	 */
+	public SQLWrapper(String databaseName, String host, String user) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+			conn = DriverManager.getConnection("jdbc:mysql://" + host + "/"
+					+ databaseName + "?" + "user=" + user);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			printSQLInformation(e);
+		} catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+	/**
+	 * Updates a value in a given table, assuming that the table has two
+	 * columns, the first of which is a string, and the second is an integer.
+	 * 
+	 * @param table
+	 *            Table to be updated
+	 * @param tag
+	 *            Tag to be updated.
+	 * @param newValue
+	 *            New value of the given tag.
+	 * @return True on success, false otherwise.
+	 */
+	public boolean updateValue(String table, String tag, int newValue) {
+		Statement statement;
+		try {
+			statement = conn.createStatement();
+			statement.executeUpdate("UPDATE " + table + " SET value="
+					+ newValue + " WHERE tag='" + tag + "'");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			printSQLInformation(e);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Inserts a value into a given table, assuming that the table has two
+	 * columns, the first of which is a string, and the second is an integer.
+	 * 
+	 * @param table
+	 *            Table to be edited.
+	 * @param tag
+	 *            First column of a table
+	 * @param value
+	 *            Second column of a table
+	 * @return True on success, false otherwise.
+	 */
+	public boolean insertIntoTable(String table, String tag, int value) {
+		Statement statement;
+		try {
+			statement = conn.createStatement();
+			statement.executeUpdate("INSERT INTO " + table + " " + "VALUES ('"
+					+ tag + "', " + value + ")");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			printSQLInformation(e);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Prints the given table to the console.
+	 * 
+	 * @param table
+	 *            Table to be printed.
+	 */
+	public void printTableInformation(String table) {
+		Statement statement;
+		ResultSet rs;
+		try {
+			statement = conn.createStatement();
+			rs = statement.executeQuery("SELECT * from " + table);
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			int numberOfColumns = rsmd.getColumnCount();
+
+			System.out.println("TABLE: " + table);
+			System.out.println("----------------");
+			for (int i = 1; i <= numberOfColumns; i++) {
+				if (i > 1)
+					System.out.print(" | ");
+				String columnName = rsmd.getColumnName(i);
+				System.out.print(columnName);
+			}
+			System.out.println("");
+			System.out.println("----------------");
+
+			while (rs.next()) {
+				for (int i = 1; i <= numberOfColumns; i++) {
+					if (i > 1)
+						System.out.print(",  ");
+					String columnValue = rs.getString(i);
+					System.out.print(columnValue);
+				}
+				System.out.println("");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			printSQLInformation(e);
+		}
+
+	}
+
+	private void printSQLInformation(SQLException ex) {
+		System.out.println("SQLException: " + ex.getMessage());
+		System.out.println("SQLState: " + ex.getSQLState());
+		System.out.println("VendorError: " + ex.getErrorCode());
+	}
+}
